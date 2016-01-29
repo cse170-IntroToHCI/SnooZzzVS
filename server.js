c33df4845711e067f366967c38270fe5d07064ae
Pablo Jacome <pabloejacome@gmail.com>
2016-01-28 19:20:55 -0800

    server is not working, changing the way the static file is produced in the browser

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('views/index.html');
+    res.render('/views/index.html');
 });
 
 app.listen(port, function() {
