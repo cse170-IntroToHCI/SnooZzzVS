17e1f4e134cae32de88a2bcf1aa8b5671068a943
Pablo Jacome <pabloejacome@gmail.com>
2016-02-04 22:25:28 -0800

    Homepage: converted the title located on topleft corner to a link to homepage Happiness History Page: converted the title located on topleft corner to a link to homepage enter goals Page: converted the title located on topleft corner to a link to homepage

---
+++ public/js/global.js
@@ -0,0 +1,8 @@
+/**
+ * Created by pablo on 2/4/2016.
+ */
+
+var globalHomeButton = document.getElementById("globalHomeButton");
+globalHomeButton.onclick = function() {
+    window.location = "./";
+};
