dc7748555a9f3c02ffaba216eb68bdce3e7d3293
pablo <pabloejacome@gmail.com>
2016-02-06 01:19:52 -0800

    added bootstrap to main page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+    res.render('home');
+});
+
+router.get('/', function(req, res) {
     res.render('index');
 });
 
