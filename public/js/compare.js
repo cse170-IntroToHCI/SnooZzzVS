83fe84c82ac19c293f49b5e9cc51c18783096e60
e3chen <emilychen101@gmail.com>
2016-02-11 15:16:03 -0800

    added compare js and css files

---
+++ public/js/compare.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	// document.getElementById("dataResults").hide();
+	$("#filterBtn").click(showResults);
+
+});
+
+function showResults() {
+	// var table = document.getElementById("dataResults");
+	// table.show();
+	// document.getElementById("dataResults").show();
+	$(".table").show('medium');
+
+}
