11850a616deadf916b23ac0898aeb9967672e1fa
e3chen <emilychen101@gmail.com>
2016-02-18 02:23:56 -0800

    fixed alarm delete

--- public/js/alarm.js
+++ public/js/alarm.js
@@ -2,28 +2,6 @@
  * Created by Emily on 2/6/2016.
  */
 
-$(document).ready(function() {
-	deleteAlarm();
-    fillData();
-
-});
-
-
-function deleteAlarm() {
-	$('button.btnDelete').on('click', function (e) {
-	    e.preventDefault();
-	    var id = $(this).closest('tr').data('id');
-	    $('#myModal').data('id', id).modal('show');
-	});
-
-	$('#btnDeleteYes').click(function () {
-	    var id = $('#myModal').data('id');
-	    $('[data-id=' + id + ']').remove();
-	    $('#myModal').modal('hide');
-	});
-}
-
-
 function addAlarmToPage(name) {
 
     var result = null;
@@ -47,7 +25,7 @@ var hourData = [];
 var minuteData = [];
 var meridiemData = "";
 var alarmHTML = "";
-// var data-id = 0;
+var dataID = 0;
 
 function fillData() {
 
@@ -55,16 +33,14 @@ function fillData() {
         hourData[i]  = extractAlarmData[i].hour;
         minuteData[i] = extractAlarmData[i].minute;
         meridiemData = extractAlarmData[i].meridiem;
-        // data-id++;
-        console.log(hourData[i]);
-        console.log(minuteData[i]);
-        console.log(meridiemData);
+        dataID++;
+        // console.log(hourData[i]);
+        // console.log(minuteData[i]);
+        // console.log(meridiemData);
+        // console.log(dataID);
 
-        // alarmHTML += '<button type="button" class="btn btn-default btn-lg" id="anAlarm">' 
-        // + hourData[i] + ":" + minuteData[i] + " " + meridiemData + '</button>' + '&nbsp;';
-        // $("#anAlarm").html(alarmHTML);
         alarmHTML += '<table id="alarmTable">' + '<tbody>' + 
-            '<tr class="btnDelete" data-id="1">' + '<td>' + 
+            '<tr class="btnDelete" data-id="' + dataID + '">' + '<td>' + 
             '<div class="btn-group" data-toggle="buttons">' + 
             '<label class="btn btn-primary active">' + 
             '<input type="radio" name="options" id="option1" autocomplete="off" checked>' + 'ON' + 
@@ -73,7 +49,7 @@ function fillData() {
             '</label>' + '</div>' + '</td>' + 
             '<td>' + '<button type="button" class="btn btn-default btn-lg" id="anAlarm">' 
         + hourData[i] + ":" + minuteData[i] + " " + meridiemData + '</button>' + '</td>' +
-            '<td>' + '<button class="btnDelete btn btn-default" href="">' + 
+            '<td>' + '<button class="btnDelete btn btn-default" href="" onClick="deleteAlarm()">' + 
             '<span class="glyphicon glyphicon-trash"></span></button>' + 
                 '</td>' + 
             '</tr>' + '</tbody>' + '</table>';
@@ -81,32 +57,25 @@ function fillData() {
 
     $("#alarmData").html(alarmHTML);
     
-
 }
 
+function deleteAlarm() {
+    $('.btnDelete').on('click', function (e) {
+        e.preventDefault();
+        var id = $(this).closest('tr').data('id');
+        $('#myModal').data('id', id).modal('show');
+    });
+
+    $('#btnDeleteYes').click(function () {
+        var id = $('#myModal').data('id');
+        $('[data-id=' + id + ']').remove();
+        $('#myModal').modal('hide');
+    });
+}
 
 window.onload = function() {
 
+    deleteAlarm();
+    fillData();
 
-    $('form').submit(function() {
-
-	 	// current Form values
-	    var hourValue = $("#selectHour").val();
-	    var minuteValue = $("#selectMinute").val();
-	    var meridiemValue = $("#selectMeridiem").val();
-
-        // Post data to JSON
-        $.ajax({
-            type: 'POST',
-            url: '/postAlarmData',
-            data: {
-                "hour": hourValue,
-                "minute": minuteValue,
-                "meridiem": meridiemValue
-            }
-        });
-
-        return false;
-    });
-
-};
+}
