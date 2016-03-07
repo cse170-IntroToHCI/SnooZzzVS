1e703730b14f1ff46399ddfeec94f369104acd51
pablo <pabloejacome@gmail.com>
2016-03-07 05:07:58 -0800

    another try at the graph. using a different example that has multiple lines included already. also has the zoom and scroll function implemented. it's only missing the ablitiy to be responsive

--- public/js/happinessHistoryGraph.js
+++ public/js/happinessHistoryGraph.js
@@ -1,196 +1,160 @@
-var margin = 60,
-    width = parseInt(d3.select("#graph").style("width")) - margin*2,
-    height = parseInt(d3.select("#graph").style("height")) - margin*2;
 
-var xScale = d3.time.scale()
+//************************************************************
+// Data notice the structure
+//************************************************************
+var data = 	[
+    [{'x':1,'y':0},{'x':2,'y':5},{'x':3,'y':10},{'x':4,'y':0},{'x':5,'y':6},{'x':6,'y':11},{'x':7,'y':9},{'x':8,'y':4},{'x':9,'y':11},{'x':10,'y':2}],
+    [{'x':1,'y':1},{'x':2,'y':6},{'x':3,'y':11},{'x':4,'y':1},{'x':5,'y':7},{'x':6,'y':12},{'x':7,'y':8},{'x':8,'y':3},{'x':9,'y':13},{'x':10,'y':3}],
+    [{'x':1,'y':2},{'x':2,'y':7},{'x':3,'y':12},{'x':4,'y':2},{'x':5,'y':8},{'x':6,'y':13},{'x':7,'y':7},{'x':8,'y':2},{'x':9,'y':4},{'x':10,'y':7}],
+    [{'x':1,'y':3},{'x':2,'y':8},{'x':3,'y':13},{'x':4,'y':3},{'x':5,'y':9},{'x':6,'y':14},{'x':7,'y':6},{'x':8,'y':1},{'x':9,'y':7},{'x':10,'y':9}],
+    [{'x':1,'y':4},{'x':2,'y':9},{'x':3,'y':14},{'x':4,'y':4},{'x':5,'y':10},{'x':6,'y':15},{'x':7,'y':5},{'x':8,'y':0},{'x':9,'y':8},{'x':10,'y':5}]
+];
+
+var colors = [
+    'steelblue',
+    'green',
+    'red',
+    'purple'
+]
+
+
+//************************************************************
+// Create Margins and Axis and hook our zoom function
+//************************************************************
+var margin = {top: 20, right: 30, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.linear()
+    .domain([0, 12])
     .range([0, width]);
-    //.nice(d3.time.day);
 
-var yScale = d3.scale.linear()
-    .range([height, 0])
-    .nice();
+var y = d3.scale.linear()
+    .domain([-1, 16])
+    .range([height, 0]);
 
 var xAxis = d3.svg.axis()
-    .scale(xScale)
-    .orient("bottom")
-    .ticks(1)
-    .tickFormat(d3.time.format("%m/%d"));
+    .scale(x)
+    .tickSize(-height)
+    .tickPadding(10)
+    .tickSubdivide(true)
+    .orient("bottom");
 
 var yAxis = d3.svg.axis()
-    .scale(yScale)
-    .ticks(1)
+    .scale(y)
+    .tickPadding(10)
+    .tickSize(-width)
+    .tickSubdivide(true)
     .orient("left");
 
-var line = d3.svg.line()
-    .x(function(d) { return xScale(d.date); })
-    .y(function(d) { return yScale(d.sleepFeeling); });
-
 var zoom = d3.behavior.zoom()
-    .x(xScale)
-    .scaleExtent([1,10])
+    .x(x)
+    .scaleExtent([1, 10])
     .on("zoom", zoomed);
 
-var graph = d3.select("#graph")
-    .attr("width", width + margin*2)
-    .attr("height", height + margin*2)
+//************************************************************
+// Generate our SVG object
+//************************************************************
+var svg = d3.select("#graph")//.append("svg")
+    .call(zoom)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform", "translate(" + margin + "," + margin + ")")
-    .call(zoom);
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json("/sleepData").get(function(err, data) {
-    if(err) {
-        console.log("Error: ");
-        console.log(err);
-    }
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
-    data.forEach(function(d) {
-        d.date = d3.time.format("%x").parse(d.date);
-        d.sleepFeeling = +d.sleepFeeling;
-    });
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
 
-    xScale.domain(d3.extent(data, function(d) { return d.date; }));
-    yScale.domain(d3.extent(data, function(d) { return d.sleepFeeling; }));
+svg.append("g")
+    .attr("class", "y axis")
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("transform", "rotate(-90)")
+    .attr("y", (-margin.left) + 10)
+    .attr("x", -height/2)
+    .text('Axis Label');
 
-    graph.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+svg.append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", width)
+    .attr("height", height);
 
-    graph.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end");
 
-    dataPerPixel = data.length/width;
-    dataResampled = data.filter(
-        function(d, i) { return i % Math.ceil(dataPerPixel) == 0; }
+
+
+
+//************************************************************
+// Create D3 line object and draw data on our SVG object
+//************************************************************
+var line = d3.svg.line()
+    .interpolate("linear")
+    .x(function(d) { return x(d.x); })
+    .y(function(d) { return y(d.y); });
+
+svg.selectAll('.line')
+    .data(data)
+    .enter()
+    .append("path")
+    .attr("class", "line")
+    .attr("clip-path", "url(#clip)")
+    .attr('stroke', function(d,i){
+        return colors[i%colors.length];
+    })
+    .attr("d", line);
+
+
+
+
+//************************************************************
+// Draw points on SVG object based on the data given
+//************************************************************
+var points = svg.selectAll('.dots')
+    .data(data)
+    .enter()
+    .append("g")
+    .attr("class", "dots")
+    .attr("clip-path", "url(#clip)");
+
+points.selectAll('.dot')
+    .data(function(d, index){
+        var a = [];
+        d.forEach(function(point,i){
+            a.push({'index': index, 'point': point});
+        });
+        return a;
+    })
+    .enter()
+    .append('circle')
+    .attr('class','dot')
+    .attr("r", 2.5)
+    .attr('fill', function(d,i){
+        return colors[d.index%colors.length];
+    })
+    .attr("transform", function(d) {
+        return "translate(" + x(d.point.x) + "," + y(d.point.y) + ")"; }
     );
 
-    graph.append("path")
-        .datum(dataResampled)
-        .attr("class", "line")
-        .attr("d", line);
-
-    var firstRecord = data[data.length-1],
-        lastRecord = data[0];
-
-    var first = graph.append("g")
-        .attr("class", "first")
-        .style("display", "none");
-
-    first.append("text")
-        .attr("x", -8)
-        .attr("y", 4)
-        .attr("text-anchor", "end")
-        .text("$" + firstRecord.sleepFeeling);
-    first.append("circle")
-        .attr("r", 4);
 
 
-    var last = graph.append("g")
-        .attr("class", "last")
-        .style("display", "none");
 
-    last.append("text")
-        .attr("x", 8)
-        .attr("y", 4)
-        .text("$" + lastRecord.sleepFeeling);
-    last.append("circle")
-        .attr("r", 4);
 
-    function resize() {
-        var width = parseInt(d3.select("#graph").style("width")) - margin*2,
-            height = parseInt(d3.select("#graph").style("height")) - margin*2;
 
-        xScale.range([0, width]).nice(d3.time.day);
-        yScale.range([height, 0]).nice().domain([1,7]);
-
-        if (width < 300 && height < 80) {
-            graph.select('.x.axis').style("display", "none");
-            graph.select('.y.axis').style("display", "none");
-
-            graph.select(".first")
-                .attr("transform", "translate(" + xScale(firstRecord.date) + "," + yScale(firstRecord.sleepFeeling) + ")")
-                .style("display", "initial");
-
-            graph.select(".last")
-                .attr("transform", "translate(" + xScale(lastRecord.date) + "," + yScale(lastRecord.sleepFeeling) + ")")
-                .style("display", "initial");
-        } else {
-            graph.select('.x.axis').style("display", "initial");
-            graph.select('.y.axis').style("display", "initial");
-            graph.select(".last")
-                .style("display", "none");
-            graph.select(".first")
-                .style("display", "none");
-        }
-
-        yAxis.ticks(Math.max(height/50, 2));
-        xAxis.ticks(Math.max(width/50, 2));
-
-        graph
-            .attr("width", width + margin*2)
-            .attr("height", height + margin*2)
-
-        graph.select('.x.axis')
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
-
-        graph.select('.y.axis')
-            .call(yAxis);
-
-        dataPerPixel = data.length/width;
-        dataResampled = data.filter(
-            function(d, i) { return i % Math.ceil(dataPerPixel) == 0; }
-        );
-
-        graph.selectAll('.line')
-            .datum(dataResampled)
-            .attr("d", line);
-
-        // stroke points
-        graph.append("g").selectAll("circle")
-            .data(data)
-            .enter()
-            .append("circle")
-            .attr("r", 5)
-            .attr("cx", function(dd){return xScale(dd.date)})
-            .attr("cy", function(dd){return yScale(dd.sleepFeeling)})
-            .attr("fill", "rgba(173, 216, 230, 0.701961)")
-            .attr("stroke", "black");
-
-        // create y label
-        graph.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y",0-margin)
-            .attr("x",0 - (height / 2))
-            .attr("dy", "3em")
-            .style("text-anchor", "middle")
-            .style("font-weight", "bold")
-            .text("Mood Values");
-
-        //d3.behavior.zoom()
-        //    .x(xScale)
-        //    .on('zoom', function() {
-        //        if(true) {
-        //
-        //        }
-        //        redrawChart();
-        //    })
-    }
-
-    d3.select(window).on('resize', resize);
-
-    resize();
-}); // end get request
-
+//************************************************************
+// Zoom specific updates
+//************************************************************
 function zoomed() {
-    graph.select(".x.axis").call(xAxis);
-    graph.selectAll("path.line").attr("d", line);
+    svg.select(".x.axis").call(xAxis);
+    // svg.select(".y.axis").call(yAxis);
+    svg.selectAll('path.line').attr('d', line);
 
-
+    points.selectAll('circle').attr("transform", function(d) {
+        return "translate(" + x(d.point.x) + "," + y(d.point.y) + ")"; }
+    );
 }
