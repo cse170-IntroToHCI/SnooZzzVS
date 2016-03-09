a04fba196b65eb7434e92cf5add6ee6e6745e4c6
pablo <pabloejacome@gmail.com>
2016-03-09 04:35:55 -0800

    graph - data from db now connected to graph. Next - change the any variables from sleepFeeling/wakeFeeling to just feeling for both

--- public/js/happinessHistoryGraph.js
+++ public/js/happinessHistoryGraph.js
@@ -2,7 +2,7 @@ var DEBUG = 0;
 var LINE_WIDTH = 6,
     DOT_RADIUS = 9;
 
-var data      = [ [{'x': '', 'y': ''}] ],
+var data      = [ [{'date': '', 'feeling': ''}] ],
     sleepData = [],
     wakeData  = [];
 //************************************************************
@@ -38,8 +38,14 @@ $.ajax({
 
 // Check to fill the data array
 if( !(sleepData.length === 0 && wakeData.length === 0) ) {
-    console.log(sleepData);
-    console.log(wakeData);
+    data = [];
+    if(DEBUG) {
+        console.log(sleepData);
+        console.log(wakeData);
+    }
+    data.push(wakeData);
+    data.push(sleepData);
+    console.log(data);
 }
 //data = 	[ //[{'x': '', 'y': ''}]
     //[{'x':'01/01/2016','y':3},{'x':'01/02/2016','y':5},{'x':'01/03/2016','y':1},{'x':'01/04/2016','y':3},{'x':'01/05/2016','y':6},{'x':'01/06/2016','y':1},{'x':'01/07/2016','y':5}],
@@ -54,8 +60,8 @@ var colors = [
 ];
 for(var i = 0; i < data.length; ++i) {
     data[i].forEach(function (d) {
-        d.x = d3.time.format("%m/%d/%Y").parse(d.x);
-        d.y = +d.y;
+        d.date = d3.time.format("%m/%d/%Y").parse(d.date);
+        d.feeling = +d.feeling;
     });
 }
 
@@ -82,10 +88,11 @@ var margin = {top: 20, right: 30, bottom: 30, left: 50},
 if(DEBUG) {
     console.log("data[0][0]");
     console.log(data[0][0]);
-    console.log(data[0][data[0].length-1].x);
+    console.log(data[0][data[0].length-1].date);
 }
-var minDate = new Date( data[0][0].x );
-var maxDate = new Date( data[0][data[0].length-1].x );
+
+var minDate = new Date( data[0][0].date );
+var maxDate = new Date( data[0][data[0].length-1].date );
 
 var xScale = d3.time.scale()
     .nice(d3.time.day)
@@ -174,8 +181,8 @@ svg.append("clipPath")
 //************************************************************
 var line = d3.svg.line()
     .interpolate("linear")
-    .x(function(d) { return xScale(d.x); })
-    .y(function(d) { return yScale(d.y); });
+    .x(function(d) { return xScale(d.date); })
+    .y(function(d) { return yScale(d.feeling); });
 
 svg.selectAll('.line')
     .data(data)
@@ -220,7 +227,7 @@ points.selectAll('.dot')
             console.log("LOOK HERE");
             console.log(d);
         }
-        return "translate(" + xScale(d.point.x) + "," + yScale(d.point.y) + ")"; }
+        return "translate(" + xScale(d.point.date) + "," + yScale(d.point.feeling) + ")"; }
     )
     .style("stroke", "black")
     .style("stroke-width", 3);
@@ -235,7 +242,7 @@ function zoomed() {
     svg.selectAll('path.line').attr('d', line);
 
     points.selectAll('circle').attr("transform", function(d) {
-        return "translate(" + xScale(d.point.x) + "," + yScale(d.point.y) + ")"; }
+        return "translate(" + xScale(d.point.date) + "," + yScale(d.point.feeling) + ")"; }
     );
 }
 
