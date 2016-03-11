e531408ca1be1f5de577d5a87d19845619d72607
pablo <pabloejacome@gmail.com>
2016-03-11 03:26:04 -0800

    sleep/wake data are being sorted when they're added to the sleep/wake data arrays wakeData PUT is now functional

--- public/js/wake.js
+++ public/js/wake.js
@@ -1,4 +1,6 @@
 
+var flipFlop = false;
+
 // default Form values
 var today = new Date();
 var theDay = (today.getMonth()+1)+"/"+today.getDate()+"/"+today.getFullYear();
@@ -15,6 +17,14 @@ function successClick() {
 }
 
 window.onload = function() {
+    // preserving html to flipflop
+    var preserveCalendar = $("#calendarContainer").html(),
+        preserveClock = $("#clockContainer").html(),
+        preserveSlider = $("#sliderContainer").html(),
+        preserveTest = $("#mainForm").html();
+
+    alertClick;
+
     // Calendar
     var datePicker = $('#datePicker').datepicker({format: 'mm/dd/yyyy'});
     datePicker.datepicker("setDate", today);
@@ -51,51 +61,132 @@ window.onload = function() {
         var meridiemValue = $("#selectMeridiem").val();
         var moodValue = mySlider.getValue();
 
-        $("#setAlert").show('medium');
-
-        // Change page layout
-
-        var newCalendarHTML = "<h3><span class='label label-info'>"+dayValue+"</span></h3>";
-        var newTimeHTML = "<h3><span class='label label-info'>"+hourValue+":"+minuteValue+" "+meridiemValue+"</span></h3>";
-        var newFeelingHTML = "<h3><span class='label label-info'>"+moodValue+"</span></h3><br>";
-        
-        $("#calendarContainer").html(newCalendarHTML);
-        $("#wakingUpAtH4").html("You woke up at:");
-        $("#clockContainer").html(newTimeHTML);
-        $("#sliderContainer").html("<h4>Your mood level was:</h4>" + newFeelingHTML);
-
-        var setButton = $("#setBtn");
-        setButton.html("Edit");
-        setButton.attr("type", "button");
-        setButton.attr("onclick", "location='/wake'");
-
-        // var cancelButton = $("#btnShow");
-        // cancelButton.html("Home");
-        // cancelButton.attr("onclick", "location='/index'");
-
-        $("#btnShow").hide();
-        // setTimeout(function() {
-        //     $("#setAlert").fadeOut();
-        // }, 2000);
-
-        // ----- EDITING LAYOUT ENDS HERE -----
-
         // Post data to JSON
         $.ajax({
-            type: 'POST',
-            url: '/wakeData',
-            data: {
-                "date": dayValue,
-                "hour": hourValue,
-                "minute": minuteValue,
-                "meridiem": meridiemValue,
-                "feeling": mySlider.getValue()
+            type: 'GET',
+            url: '/wakeData/search?date='+dayValue,
+            success: function(req) {
+                // Data Not Found
+                if(req === null || req === undefined || req === "") {
+                    // Post data to JSON
+                    $.ajax({
+                        type: 'POST',
+                        url: '/wakeData',
+                        data: {
+                            "date": dayValue,
+                            "hour": hourValue,
+                            "minute": minuteValue,
+                            "meridiem": meridiemValue,
+                            "feeling": mySlider.getValue()
+                        },
+                        success: function() {
+                            $("#setAlert").show('medium');
+                            $("#btnShow").hide();
+
+                            //************************************************************
+                            // Update Page Layout
+                            //************************************************************
+                            var newCalendarHTML = "<h3><span class='label label-info'>"+dayValue+"</span></h3>";
+                            var newTimeHTML = "<h3><span class='label label-info'>"+hourValue+":"+minuteValue+" "+meridiemValue+"</span></h3>";
+                            var newFeelingHTML = "<h3><span class='label label-info'>"+moodValue+"</span></h3><br>";
+
+                            $("#calendarContainer").html(newCalendarHTML);
+                            $("#wakeingAtH4").html("You went to wakeData at:");
+                            $("#clockContainer").html(newTimeHTML);
+                            $("#sliderContainer").html("<h4>Your mood level was:</h4>" + newFeelingHTML);
+
+
+                            var setButton = $("#setBtn");
+                            setButton.html("Edit")
+                                .attr("id", "editBtn")
+                                .attr("type", "button");
+
+                            flipFlop = true;
+                            // ----- EDITING LAYOUT ENDS HERE -----
+                        }
+                    });
+                    // Data already Exists
+                } else {
+                    $("#myModal").modal('show');
+                    $("#yesReplaceLog").click(function() {
+                        $("#myModal").modal('hide');
+
+                        $.ajax({
+                            type: 'PUT',
+                            url: '/wakeData',
+                            data: {
+                                date: $("#selectDate").val(),
+                                hour: $("#selectHour").val(),
+                                minute: $("#selectMinute").val(),
+                                meridiem: $("#selectMeridiem").val(),
+                                feeling: $("#happinessSlider").val()
+                            },
+                            success: function() {
+                                //alert("Log Updated!"); todo - button code below in here
+                                $("#setAlert").show('medium');
+                                $("#btnShow").hide();
+
+                                //************************************************************
+                                // Update Page Layout
+                                //************************************************************
+                                var newCalendarHTML = "<h3><span class='label label-info'>"+dayValue+"</span></h3>";
+                                var newTimeHTML = "<h3><span class='label label-info'>"+hourValue+":"+minuteValue+" "+meridiemValue+"</span></h3>";
+                                var newFeelingHTML = "<h3><span class='label label-info'>"+moodValue+"</span></h3><br>";
+
+                                $("#calendarContainer").html(newCalendarHTML);
+                                $("#wakingUpAtH4").html("You woke up at:");
+                                $("#clockContainer").html(newTimeHTML);
+                                $("#sliderContainer").html("<h4>Your mood level was:</h4>" + newFeelingHTML);
+
+                                var setButton = $("#setBtn");
+                                setButton.html("Edit")
+                                    .attr("id", "editBtn")
+                                    .attr("type", "button");
+
+                                flipFlop = true;
+                                // ----- EDITING LAYOUT ENDS HERE -----
+                            }
+                        });
+                    });
+                }
             }
         });
 
         return false;
     });
 
-    // Button Click Functions
-    $("#btnShow").click(alertClick);
+    $("#setBtn").click(function() {
+        if(flipFlop === true) {
+            $("#setAlert").hide('medium');
+            $("#wakingUpAtH4").html("Waking up at:");
+            $("#clockContainer").html(preserveClock);
+            $("#sliderContainer").html(preserveSlider);
+
+            $(this).html("Update")
+                .attr("id", "editBtn")
+                .attr("type", "button");
+
+            new Slider("#happinessSlider", {});
+            flipFlop = 2;
+        } else if(flipFlop === 2) {
+            $.ajax({
+                type: 'PUT',
+                url: '/wakeData',
+                data: {
+                    date: $("#calendarContainer h3 span").html(),
+                    hour: $("#selectHour").val(),
+                    minute: $("#selectMinute").val(),
+                    meridiem: $("#selectMeridiem").val(),
+                    feeling: $("#happinessSlider").val()
+                },
+                success: function() {
+                    $("#sleepUpdateSuccessModal").modal('show');
+                }
+            });
+        }
+    });
+
+    $("#okayButton").click(function() {
+        window.location = './index';
+    });
 };
--- routes/sleepData/sleepData.js
+++ routes/sleepData/sleepData.js
@@ -3,28 +3,53 @@ var db = require('../../db');
 var ObjectId = require('mongodb').ObjectID;
 module.exports.sleepData = {};
 
+function compare(a,b) {
+    if (a.date < b.date)
+        return -1;
+    else if (a.date > b.date)
+        return 1;
+    else
+        return 0;
+}
+
 module.exports.POST = function(req, res) {
-    var date        = req.body.date;
+    var thedate     = req.body.date;
     var hour        = req.body.hour;
     var minute      = req.body.minute;
     var meridiem    = req.body.meridiem;
     var feeling     = req.body.feeling;
-    var sess = req.session;
+    var sess        = req.session;
 
     var newSleepData = {
-        "date": date,
+        "date": thedate,
         "hour": hour,
         "minute": minute,
         "meridiem": meridiem,
         "feeling": feeling
     };
 
+    // must use update when pushing to an array
     var sleepDataCollection = db.get().collection('sleepData');
     sleepDataCollection.update(
         {_id: ObjectId(sess.sleepObjectId)},
-        {$push: {sleepData: newSleepData}}
+        {$push: {
+                sleepData: {
+                    $each: [newSleepData],
+                    $sort: {date: 1}
+                }
+            }
+        },
+        function(err, result) {
+            if(err) {
+                console.log("Error Posting Sleep Data");
+                console.log(err);
+                return res.status(400).end();
+            } else {
+                console.log("Posted Sleep Data");
+                return res.status(200).end();
+            }
+        }
     );
-    return res.status(200).end();
 };
 
 module.exports.GET = function(req, res) {
@@ -43,71 +68,25 @@ module.exports.GET = function(req, res) {
 
 module.exports.PUT    = function(req, res) {
     var date = req.body.date;
-    var updateData = {
-        date: date,
-        hour: req.body.hour,
-        minute: req.body.minunte,
-        meridiem: req.body.meridiem,
-        feeling: req.body.feeling
-    };
-
     var sleepObjectId = req.session.sleepObjectId;
     var sleepDataCollection = db.get().collection('sleepData');
-    console.log("req.body",req.body);
     sleepDataCollection.find({_id: ObjectId(sleepObjectId)}).toArray(function(err, sleepDataObjectArray) {
         if(err) {
             console.log("Error-Sleep Data Error@GET: " + err);
             return res.status(400).end();
         } else {
-            console.log("sleepDataObjectArray",sleepDataObjectArray);
-            console.log("******************************");
-            console.log("sleepDataObjectArray[0].sleepData", sleepDataObjectArray[0].sleepData);
-            console.log("******************************");
             for(targetDate in sleepDataObjectArray[0].sleepData) {
-                console.log("targetDate:",targetDate);
                 if(new Date(sleepDataObjectArray[0].sleepData[targetDate].date).getTime() === new Date(date).getTime()) {
-
-                    //console.log("FOUND IT BEFORE: ", sleepDataObjectArray[0].sleepData[targetDate]);
-                    //sleepDataObjectArray[0].sleepData[targetDate] = updateData;
-                    //console.log("FOUND IT AFTER : ", sleepDataObjectArray[0].sleepData[targetDate]);
-                    ////sleepDataCollection.save(
-                    ////    {
-                    ////        date: date,
-                    ////        hour: ,
-                    ////        minute: ,
-                    ////        meridiem: ,
-                    ////        feeling
-                    ////    }
-                    ////);
-                    console.log("############################");
-
-                    sleepDataCollection.find({_id: ObjectId(sleepObjectId), "sleepData.date": date}).toArray(function(err, wtf) {
-                        console.log("err:",err);
-                        console.log("wtf:",wtf);
-                    });
-
-                    sleepDataCollection.find({_id: ObjectId(sleepObjectId)}).toArray(function(err1, wtf1) {
-                        console.log("err1:",err1);
-                        console.log("wtf1:",wtf1);
-                    });
-
-                    sleepDataCollection.find({"sleepData.date": date}).toArray(function(err2, wtf2) {
-                        console.log("err2:",err2);
-                        console.log("wtf2:",wtf2);
-                    });
-
-                    sleepDataCollection.update({_id: ObjectId(sleepObjectId), "sleepData.date": date}, {$set: {"sleepData.$" : req.body}}, function(err, update) {
+                    sleepDataCollection.update(
+                        {_id: ObjectId(sleepObjectId), "sleepData.date": date}, // query
+                        {$set: {"sleepData.$" : req.body}},                     // update
+                        function(err, update) {
                         if(err) {
                             console.log("Update Sleep Data Failed");
                             console.log(err);
                             return res.status(400).end();
-                        } else if(!update) {
-                            console.log("Nothing found to update");
-                            return res.status(200).end();
                         } else {
                             console.log("Sleep Data Updated");
-                            console.log("+++++++++++++++++++++++++");
-                            //console.log("update: ", update);
                             return res.status(200).send("Sleep Data Updated").end();
                         }
                     });
